483b6cc8422a6cf31bcefce78ec20ddbaa2bddca
Sarah Morris <sarah.morris9@gmail.com>
2017-11-30 10:05:02 -0600

    Added css and html divs

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 console.log("ms1 review");
 
+let dogContent = document.getElementById("dogs");
+let rainbowContent = document.getElementById("rainbows");
+
+// dynamically create some elements to insert pics into
+// make n sections: n is based on number of imgs
+// insert the pictures 
+// loop through images and create sections one at a time
+
+for (let i=0; i < somePics; i++) {
+  
+}
 
